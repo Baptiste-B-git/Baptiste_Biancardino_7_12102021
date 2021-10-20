b0ba31801ede11d3d76a88c5281d35f72d9763ca
unknown <baptiste.biancardino@gmail.com>
2021-10-20 15:45:14 +0200

    création des routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,18 @@
-const UserModel = require('../models/user');
+const UserModel = require('../models').User;
 
 module.exports.signUp = async (req, res) => {
     console.log(req.body);
-    const {pseudo, email, password} = req.body
+    const {email, username, password, bio, isAdmin} = req.body
 
     try {
-        const user = await UserModel.create({pseudo, email, password });
-        res.status(201).json({ user: user._id})
+        const user = await UserModel.create({
+            email,
+            username,
+            password,
+            bio,
+            isAdmin
+        });
+        res.status(201).json({user})
     }
     catch(err) {
         res.status(200).send({ err })
