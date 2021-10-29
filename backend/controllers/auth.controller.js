76cdacf83a4f7dcb4523928984666b9a8d49b6eb
unknown <baptiste.biancardino@gmail.com>
2021-10-29 13:31:37 +0200

    Configuration du login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,49 +1,16 @@
 const UserModel = require('../models').User;
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-// création du token et expiration + décodage du token
-// maxAge= temps avant déconnexion du user
-const maxAge = 3 * 24 * 60 * 60 * 1000;
-const createToken = (id) => {
-    return jwt.sign({id}, process.env.RANDOM_TOKEN_SECRET, {
-        expiresIn: maxAge
-    })
-};
-
-
-// (cours) JWT Login
-exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-      .then(user => {
-        if (!user) {
-          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-        }
-        bcrypt.compare(req.body.password, user.password)
-          .then(valid => {
-            if (!valid) {
-              return res.status(401).json({ error: 'Mot de passe incorrect !' });
-            }
-            res.status(200).json({
-              userId: user._id,
-              token: jwt.sign(
-                { userId: user._id },
-                'RANDOM_TOKEN_SECRET',
-                { expiresIn: '24h' }
-              )
-            });
-          })
-          .catch(error => res.status(500).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
-  };
-// JWT login
 
 
 module.exports.signUp = async (req, res) => {
     console.log(req.body);
-    const {email, username, password, bio, isAdmin} = req.body
+    let {email, username, password, bio, isAdmin} = req.body
+      const salt = await bcrypt.genSalt();
 
     try {
+      password = await bcrypt.hash(password, salt);
         const user = await UserModel.create({
             email,
             username,
@@ -54,23 +21,67 @@ module.exports.signUp = async (req, res) => {
         res.status(201).json({user})
     }
     catch(err) {
+      console.log(err);
         res.status(200).send({ err })
     }
 }
 
 module.exports.signIn = async (req, res) =>{
-    const {email, password} = req.body
-
-    try{
-        const user = await UserModel.login(email, password);
-        const token = createToken(user._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge:maxAge});
-        res.status(200).json({ user: user._id})
-    } catch(err){
-        res.status(200).json(err);
+  console.log(1);
+  UserModel.findOne({where:{ email: req.body.email }})
+  .then(user => {
+    console.log(user);
+    if (!user) {
+      return res.status(401).json({ error: 'Utilisateur non trouvé !' });
     }
+    bcrypt.compare(req.body.password, user.password)
+      .then(valid => {
+        console.log(3);
+        if (!valid) {
+          return res.status(401).json({ error: 'Mot de passe incorrect !' });
+        }
+        res.status(200).json({
+          userId: user.id,
+          token: jwt.sign(
+            { userId: user.id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' }
+          )
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
+  })
+  .catch(error => res.status(500).json({ error }));
 }
 
 module.exports.logout = (req, res) =>{
 
 }
+
+
+// // // (cours) JWT Login
+// // exports.login = (req, res, next) => {
+//     User.findOne({ email: req.body.email })
+//       .then(user => {
+//         if (!user) {
+//           return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+//         }
+//         bcrypt.compare(req.body.password, user.password)
+//           .then(valid => {
+//             if (!valid) {
+//               return res.status(401).json({ error: 'Mot de passe incorrect !' });
+//             }
+//             res.status(200).json({
+//               userId: user._id,
+//               token: jwt.sign(
+//                 { userId: user._id },
+//                 'RANDOM_TOKEN_SECRET',
+//                 { expiresIn: '24h' }
+//               )
+//             });
+//           })
+//           .catch(error => res.status(500).json({ error }));
+//       })
+//       .catch(error => res.status(500).json({ error }));
+// //   };
+// // // JWT login
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const bcrypt = require('bcrypt');
-
 const {
   Model
 } = require('sequelize');
@@ -26,27 +24,6 @@ module.exports = (sequelize, DataTypes) => {
   return User;
 };
 
-// // crypter le password
-// const bcrypt = require('bcrypt');
 
-// userSchema.pre("save", async function(next){
-//   const salt = await bcrypt.genSalt();
-//   this.password = await bcrypt.hash(this.password, salt);
-//   next();
-// });
-
-// // auth.controller.js
-// // Au moment de se log, récupération de l'email et du password puis bcrypt compare les deux crypt et il valide si c'est bon
-// user.Schema.statics.login = async function (email, password){
-//   const user = await this.findOne ({ email });
-//   if (user){
-//     const auth = await bcrypt.compare(password, user.password);
-//     if (auth){
-//       return user;
-//     }
-//     throw Error('incorrect password');
-//   }
-//   throw Error('incorrect email')
-// };
 
 
