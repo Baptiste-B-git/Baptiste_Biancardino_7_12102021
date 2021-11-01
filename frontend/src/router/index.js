8ac3ddc227fa9e57746e9d1ccb154e377490fa77
unknown <baptiste.biancardino@gmail.com>
2021-11-01 15:42:54 +0100

    Front axios connexion et inscription

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,26 +1,24 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createWebHistory, createRouter } from "vue-router";
+import Login from "@/views/Login.vue";
+import Profile from "@/views/Profile.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
+  { 
+    name: 'login',
+    path: '/', 
+    component: Login,
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  { 
+    name: 'profile',
+    path: '/profile', 
+    component: Profile, 
+    props:true 
   },
-];
+]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
-});
+})
 
 export default router;
